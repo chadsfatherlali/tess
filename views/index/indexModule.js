e5971405d478a14704cae2b6126cdb8ef85331b6
Chasfather <chasfather@gmail.com>
2015-03-07 15:56:23 +0100

    Prueba de PHPStorm, mola :)

--- views/index/indexModule.js
+++ views/index/indexModule.js
@@ -6,13 +6,13 @@ exports.index = function() {
             var mongoose = require("mongoose");
             var modelos = require("../../modelos");
             var Q = require("q");
-
+            
             var p0 = modelos.modelo("AltaProducto").find().exec();
             var p1 = modelos.modelo("TipoProducto").find({}, {producto: 1, _id: 0}).exec();
             var p2 = modelos.modelo("Categoria").find({}, {categoria: 1, _id: 0}).exec();
-
+            
             var allPromise = Q.all([p0, p1, p2]);
-
+            
             allPromise.then(function(response) { 
                 if(callback) callback(response);
             },
